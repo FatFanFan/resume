889c2b6fd83157e8ee4b25566a54d51841c1e049
fatfan <fatfan1020@gmail.com>
2018-06-25 16:18:43 +0800

    增加留言功能

--- js/message.js
+++ js/message.js
@@ -1,79 +1,39 @@
-!function(){
-  var model = {
-    // 获取数据
-    init: function(){
-      var APP_ID = 'TsDnap9SEXjSvGSowP7gXXJC-gzGzoHsz'
-      var APP_KEY = 'rGye31p12mM3wFpNRn9RADu9'
-      AV.init({ appId: APP_ID, appKey: APP_KEY })
-    },
-    fetch: function(){ 
-      var query = new AV.Query('Message');
-      return query.find() // Promise 对象
-    },
-    // 创建数据
-    save: function(name, content){
-      var Message = AV.Object.extend('Message');
-      var message = new Message();
-      return message.save({  // Promise 对象
-        'name': name,
+var APP_ID = 'ubx0q7XPyH4msGdUihffS62M-gzGzoHsz';
+var APP_KEY = 'qV3mA085Qz9IXlBmH7BC2bjr';
+
+AV.init({
+    appId: APP_ID,
+    appKey: APP_KEY
+});
+
+var query =new AV.Query('Message');
+query.find()
+    .then(
+        function(messages){
+            let array =messages.map((item)=>item.attributes)
+            array.forEach((item)=>{
+                let li =document.createElement('li')
+                li.innerText = `${item.name}:${item.content}`
+                let messageList =document.querySelector('#messageList')
+                messageList.appendChild(li)
+            })
+        }
+    )
+let myForm = document.querySelector('#postMessageForm')
+myForm.addEventListener('submit', function(e) {
+    e.preventDefault()
+    let name = myForm.querySelector('input[name=name]').value
+    let content = myForm.querySelector('input[name=content]').value
+    var Message = AV.Object.extend('Message');
+    var message = new Message();
+    message.save({
+        'name':name,
         'content': content
-      })
-    }
-  }
-
-  var view = document.querySelector('section.message')
-
-
-  var controller = {
-    view: null,
-    model: null,
-    messageList: null,
-    init: function(view, model){
-      this.view = view
-      this.model = model
-
-      this.messageList = view.querySelector('#messageList')
-      this.form = view.querySelector('form')
-      this.model.init()
-      this.loadMessages()
-      this.bindEvents()
-    },
-    loadMessages: function(){
-      this.model.fetch().then(
-        (messages) => {
-          let array = messages.map((item)=> item.attributes )
-          array.forEach((item)=>{
-            let li = document.createElement('li')
-            li.innerText = `${item.name}: ${item.content}`
-            this.messageList.appendChild(li)
-          })
-        } 
-      )
-    },
-    bindEvents: function(){
-      this.form.addEventListener('submit', function(e){
-        e.preventDefault()
-        this.saveMessage()
-      })
-    },
-    saveMessage: function(){
-      let myForm = this.form
-      let content = myForm.querySelector('input[name=content]').value
-      let name = myForm.querySelector('input[name=name]').value
-      this.model.save(name, content).then(function(object) {
-        let li = document.createElement('li')
-        li.innerText = `${object.attributes.name}: ${object.attributes.content}`
-        let messageList = document.querySelector('#messageList')
+    }).then(function(object){
+        let li =document.createElement('li')
+        li.innerText = `${object.attributes.name}:${object.attributes.content}`
+        let messageList =document.querySelector('#messageList')
         messageList.appendChild(li)
-        myForm.querySelector('input[name=content]').value = ''
         console.log(object)
-      })
-    }
-
-  }
-
-  controller.init(view, model)
-
-
-}.call()
-
+    })
+})
